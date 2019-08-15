e94bffa15b03ac6093c558f22c851e75f520227e
Rob Martin <rob@rankcrankers.com>
2019-08-15 16:39:28 -0500

    CS Vid 2

---
+++ flask_blog/flaskblog.py
@@ -0,0 +1,37 @@
+# PowersHell >> $env:FLASK_APP = "flaskblog.py"
+#            >> $env:FLASK_ENV = "development"
+# https://youtu.be/QnDWIZuWYW0?t=622
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Rob Martin",
+        "tite": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 15, 2019",
+    },
+    {
+        "author": "Rod Martin",
+        "tite": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 15, 2019",
+    },
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
