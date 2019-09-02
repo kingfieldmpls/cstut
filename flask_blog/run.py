1b42041439b8f89e96a07764340de46891b5c4dd
Rob Martin <martinrobd@gmail.com>
2019-09-02 11:19:03 -0500

    Part 6 complete

--- flask_blog/run.py
+++ flask_blog/run.py
@@ -1,9 +1,7 @@
 # PowersHell >> $env:FLASK_APP = "flaskblog.py"
 #            >> $env:FLASK_ENV = "development"
-# YouTube link: https://www.youtube.com/watch?v=44PvX0Yv368
-# I don't understand import routes in __init__
-# I don't understand import models in routes.py
-# Next vid (after rewatching for comprehension) https://www.youtube.com/watch?v=CSHx6eCkmv0
+# YouTube link: https://www.youtube.com/watch?v=803Ei2Sq-Zs
+
 from flaskblog import app
 
 if __name__ == "__main__":
